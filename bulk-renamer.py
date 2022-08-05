abf93482c6c8d2d29a3384793217410328a8f27a
theforbiddenpool <contact@theforbiddenpool.com>
2022-08-05 15:00:38 +0200

    feat(renamer): check if the os is linux

--- bulk-renamer.py
+++ bulk-renamer.py
@@ -1,15 +1,22 @@
-import os
+from sys import platform
+
+def is_linux():
+  if not platform.startswith('linux'):
+    exit("OS not supported")
+
 
 def main():
-  i = 0
-  path = ""
-  for filename in os.listdir(path):
-    my_dest = "img" + str(i) + ".jpg"
-    my_source = path + filename
-    my_dest = path + my_dest
-    os.rename(my_dest, my_source)
-    i += 1
+  is_linux()
+  
+  # i = 0
+  # path = ""
+  # for filename in os.listdir(path):
+  #   my_dest = "img" + str(i) + ".jpg"
+  #   my_source = path + filename
+  #   my_dest = path + my_dest
+  #   os.rename(my_dest, my_source)
+  #   i += 1
 
-if __name__ == '__main__': # run main function when program is called
+if __name__ == '__main__':
     main()
 
