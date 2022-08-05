1d11a3975a284ff49aaba70275eafdd4fcba1e01
theforbiddenpool <contact@theforbiddenpool.com>
2022-08-05 15:22:31 +0200

    feat(renamer): ask for path and if all files should be renamed

--- bulk-renamer.py
+++ bulk-renamer.py
@@ -1,12 +1,31 @@
+import os
 from sys import platform
 
 def is_linux():
   if not platform.startswith('linux'):
     exit("OS not supported")
 
-
 def main():
   is_linux()
+
+  try:
+    path = input("Path to directory: ")
+    
+    if not os.path.exists(path):
+      raise Exception("Path doesn't exist")
+    
+    while True:
+      rename_all_files = input("Rename all files? [Y/n] ").lower()
+      if rename_all_files == 'y':
+        print("yes!")
+        break
+      elif rename_all_files == 'n':
+        print("no!")
+        break
+      else:
+        print("\nPlease only enter Y or N!")
+  except Exception as error:
+    print(error)
   
   # i = 0
   # path = ""
